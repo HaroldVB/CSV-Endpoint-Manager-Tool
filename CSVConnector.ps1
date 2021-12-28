8b4fa3f02a2981e050472c60e44ca2f34ec71bce
Lukas <73724671+HaroldVB@users.noreply.github.com>
2021-12-28 21:35:50 +0100

    Update CSVConnector.ps1

--- CSVConnector.ps1
+++ CSVConnector.ps1
@@ -1,12 +1,42 @@
+# Setting mandatory parameters for script to work
 param (
     [Parameter(Mandatory=$true)]
     [ValidateNotNullOrEmpty()]
     [String]$outputdir, $inputdir
 )
 
-Get-ChildItem -Path $inputdir -Filter *.csv | 
-Foreach-Object {
+# Set ErrorActionPreferenc for correct output
+$ErrorActionPreference = "Stop"
+
+# Checking if the input directory exists
+$inputdirpresent = (Test-Path -Path $inputdir)
+If ($inputdirpresent -eq "true") {
+    Write-Host "Started combining CSV files..." -ForegroundColor Green
+}
+else {
+    Write-Host "Could not find input directory at $inputdir" -ForegroundColor Red
+    exit
+}
+
+# Appending CSV files and creating a masterfile
+Try {   
+    Get-ChildItem -Path $inputdir -Filter *.csv | 
+    Foreach-Object {
     $fileNames = Get-ChildItem -Path $inputdir -Recurse -Include *.csv
-    Import-Csv $fileNames | Export-Csv $outputdir\InTuneDevices.csv -notype 
-    break
+    Import-Csv $fileNames | Export-Csv $outputdir\InTuneDevices.csv -notype
+    }
+}
+Catch {
+    Write-Host "Not able to append CSV files to masterfile. Please check inputs."
+    exit
+}
+
+# Checking if script was successful
+$outputfile = Test-Path -Path $outputdir\InTuneDevices.csv
+if ($outputfile -eq "true") {
+    Write-Host "Successfully created CSV masterfile at $outputdir" -ForegroundColor Green
+}
+
+else {
+    Write-Host "CSV file was not created." -ForegroundColor Red
 }
